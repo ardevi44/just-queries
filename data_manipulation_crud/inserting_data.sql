19f802a0ea3fa1bd890d9aa6cb5a8081831e7a05
ardevi44 <arbey9621@gmail.com>
2026-01-26 04:09:27 -0600

    inserting wrong data in sales

--- data_manipulation_crud/inserting_data.sql
+++ data_manipulation_crud/inserting_data.sql
@@ -29,4 +29,22 @@ VALUES (
     400000.0,
     FALSE,
     TRUE
+  );
+
+INSERT INTO sales (
+    date_created,
+    date_fulfilled,
+    customer_name,
+    product_name,
+    volume
   )
+VALUES (
+    "2021-12-09",
+    "2021-12-17",
+    "Company Z",
+    "A tuck",
+    109.0
+  );
+
+SELECT *
+FROM sales;
