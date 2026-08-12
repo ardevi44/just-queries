c751ce04e6d0a47e9787f21e05bf3c3ce4e43adc
ardevi44 <arbey9621@gmail.com>
2026-08-12 16:28:04 -0600

    Inserting data in users table

--- 03-insert_users_data.sql
+++ 03-insert_users_data.sql
@@ -1,9 +1,17 @@
 DESCRIBE users;
 
-INSERT INTO users (yearly_salary, full_name, current_status)
-VALUES (19000, 'Max Schwarz', 'self-employed');
+/*
+ Remember use "" doubles for identifiers in postgresql
+ and '' single for string data
+ */
+INSERT INTO
+  users (yearly_salary, full_name, current_status)
+VALUES
+  (19000, 'Max Schwarz', 'self-employed');
 
-INSERT INTO users (yearly_salary, full_name, current_status)
-VALUES (0, "Fabricio Guerra", 'unemployed'),
-  (25000, "Marisol Gonzalez", "self-employed"),
-  (35000, "Ardevi44", "employed");
+INSERT INTO
+  users (yearly_salary, full_name, current_status)
+VALUES
+  (0, 'Fabricio Guerra', 'unemployed'),
+  (25000, 'Marisol Gonzalez', 'self-employed'),
+  (35000, 'rdevi44', 'employed');
