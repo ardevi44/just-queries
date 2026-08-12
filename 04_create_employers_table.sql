34c0d6896ff60588aee5f13b2b90c5ab87a720b0
ardevi44 <arbey9621@gmail.com>
2026-08-12 16:27:26 -0600

    Cretae employers table

---
+++ 04_create_employers_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE employers (
+  company_name VARCHAR(250),
+  company_address VARCHAR(300),
+  -- yearly_revenue FLOAT(5,2), This could be rounded because of float
+  -- NUMERIC exact value
+  yearly_revenue NUMERIC(5, 2),
+  is_hiring BOOLEAN
+);
