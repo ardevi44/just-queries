8b6b68de846cfd11c01341b13c80c54b75838fd7
ardevi44 <arbey9621@gmail.com>
2026-08-12 02:08:08 -0600

    change the name of some files

--- create_db_talently.sql
+++
@@ -1,3 +0,0 @@
-CREATE DATABASE Talently;
-
-USE Talently;
--- create_users_table.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE users (
-  full_name VARCHAR(200),
-  yearly_salary INT,
-  current_status ENUM('employed', 'self-employed', 'unemployed')
-);
