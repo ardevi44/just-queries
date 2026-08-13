ebb1f135de1dd3bbe34e9965ab6ea16c029de261
ardevi44 <arbey9621@gmail.com>
2026-08-13 04:01:58 -0600

    Inserting data in conversations and employers

---
+++ 06_insert_data_employers.sql
@@ -0,0 +1,26 @@
+INSERT INTO
+  employers (
+    company_name,
+    company_address,
+    yearly_revenue,
+    is_hiring
+  )
+VALUES
+  (
+    'Learning Inc',
+    'Educationstreet 12, London',
+    0.87,
+    TRUE
+  ),
+  (
+    'Big Oil Inc',
+    'Slipperystreet 110, Houston',
+    112.55,
+    FALSE
+  ),
+  (
+    'Hipster Food',
+    'Avocadostreet 5, Berlin',
+    6.12,
+    TRUE
+  );
---
+++ 07_insert_conversations.sql
@@ -0,0 +1,27 @@
+INSERT INTO
+  conversations (
+    user_name,
+    employer_name,
+    message,
+    date_sent
+  )
+VALUES
+  (
+    'Max Schwartz',
+    'Learning Inc',
+    'Hi, I like learning',
+    '2026-08-12 16:32:10'
+  );
+
+INSERT INTO
+  conversations (
+    user_name,
+    employer_name,
+    message
+  )
+VALUES
+  (
+    'Max Schwartz',
+    'Learning Inc',
+    'I want to learn something'
+  );
