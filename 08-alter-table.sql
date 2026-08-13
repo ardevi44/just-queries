5ef8db654d1326948264561fefe6a076706b4f4a
ardevi44 <arbey9621@gmail.com>
2026-08-13 05:09:06 -0600

    Altering columns table

--- 08-alter-table.sql
+++ 08-alter-table.sql
@@ -12,3 +12,18 @@ ALTER COLUMN
   date_sent
 SET
   DEFAULT CURRENT_TIMESTAMP;
+
+-- CHANGE A COLUMN DATA TYPE in postgresql
+ALTER TABLE
+  employers
+ALTER COLUMN
+  yearly_revenue
+SET
+  DATA TYPE FLOAT;
+
+ALTER TABLE
+  users
+ALTER COLUMN
+  full_name
+SET
+  DATA TYPE VARCHAR(300);
