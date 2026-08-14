4a929308435756102bab9add43b639eeb0cddd61
ardevi44 <arbey9621@gmail.com>
2026-08-14 16:20:55 -0600

    Adding a check constraint in a column

--- 13-check-constraint.sql
+++ 13-check-constraint.sql
@@ -3,28 +3,15 @@ ALTER TABLE
 ADD
   CHECK (yearly_salary > 0);
 
-SELECT
-  *
-FROM
-  users;
-
-UPDATE
-  users
-SET
-  yearly_salary = 10000
-WHERE
-  full_name = 'Fabricio Guerra';
-
-INSERT INTO
-  users (full_name, yearly_salary, current_status)
-VALUES
-  ('Irvinn Everardo', 9000, 'employed');
-
+/*If some of the rows is actually violating this rule the query is gonna get an error,
+ because of it*/
+-- This is a query to show the constraints and their names
 SELECT
   constraint_name,
   check_clause
 FROM
   information_schema.check_constraints;
 
+-- And this is a way to drop the constraints
 ALTER TABLE
   users DROP CONSTRAINT users_yearly_salary_check;
